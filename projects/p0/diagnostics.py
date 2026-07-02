8293d9b1810ae5df502d89838b7238ed22aa3221
Anonemous2 <AndrewFBates@icloud.com>
2026-07-02 13:47:34 -0400

    Tested DB

--- projects/p0/diagnostics.py
+++ projects/p0/diagnostics.py
@@ -1,4 +1,5 @@
 import subprocess
+import re
 
 # Project 0 modules:
 import options
@@ -11,6 +12,52 @@ class Diagnostics():
 
     def run_diagnostics(self):
         self.run_usage()
+
+    def run_cpu(self, usage):
+        # Using free --human, we can get the total RAM and Swap memory 
+        # avaliable.
+        proc_info = subprocess.Popen(["cat", "/proc/cpuinfo"], 
+                                stdout=subprocess.PIPE, 
+                                stderr=subprocess.STDOUT)
+        proc_speed = subprocess.Popen(["grep", "MHz"], 
+                                stdin=proc_info.stdout, 
+                                stdout=subprocess.PIPE, 
+                                stderr=subprocess.STDOUT)
+        proc_cores = subprocess.Popen(["grep", "siblings"], 
+                                stdin=proc_info.stdout, 
+                                stdout=subprocess.PIPE, 
+                                stderr=subprocess.STDOUT)
+        proc_name = subprocess.Popen(["grep", "model name"], 
+                                stdin=proc_info.stdout, 
+                                stdout=subprocess.PIPE, 
+                                stderr=subprocess.STDOUT)
+        # Check if the command failed to run correctly.
+        if proc_speed.returncode != 0:
+            Exception(f"Error running 'top': {proc_speed.returncode}")
+        output_speed = proc_speed.communicate()
+
+        if proc_cores.returncode != 0:
+            Exception(f"Error running 'top': {proc_cores.returncode}")
+        output_cores = proc_cores.communicate()
+
+        if proc_name.returncode != 0:
+            Exception(f"Error running 'top': {proc_name.returncode}")
+        output_name = proc_name.communicate()
+
+        # TODO: Error handling (unexpected re failures).
+        text_name = re.split(r'\n', output_name[0].decode())
+        text_name = re.search(r'^model name\s+:\s+(.+)', text_name[0]).group(1)
+        
+        text_cores = re.split(r'\n', output_cores[0].decode())
+        text_cores = re.search(r'^siblings\s+:\s+(\d+)', text_cores[0]).group(1)
+        
+        text_speed = re.split(r'\n', output_speed[0].decode())
+        text_speed = round(float(re.search(r'^cpu MHz\s+:\s+(\S+)', text_speed[0]).group(1)) / 1000, 2)
+
+        print(f'CPU: {text_name}')
+        display_cpu = f'{usage}% CPU usage, {text_cores} logical processors at {text_speed} GHz'
+        print(display_cpu)
+
         
     def run_usage(self):
         # Using free --human, we can get the total RAM and Swap memory 
@@ -21,35 +68,61 @@ class Diagnostics():
         # Check if the command failed to run correctly.
         if proc.returncode != 0:
             Exception(f"Error running 'top': {proc.returncode}")
-
-        # TODO: Log procs/process command output.
         output = proc.communicate()
-        print(output[0].decode())
 
-    def run_memory(self):
-        # Using free --human, we can get the total RAM and Swap memory 
-        # avaliable.
-        proc = subprocess.Popen(["free", '-h'], 
-                                  stdout=subprocess.PIPE, 
-                                  stderr=subprocess.STDOUT)
-        # Check if the command failed to run correctly.
-        if proc.returncode != 0:
-            Exception(f"Error running 'free -h': {proc.returncode}")
+        # TODO: Error handling (unexpected re failures).
+        text_out = re.split(r'\n', output[0].decode())
 
-        # TODO: Log procs/process command output.
-        # output = proc.communicate()
-        # print(output[0].decode())
+        reg_ex = r'^top - (\S+).*, +\d user, +load average: (\S+), (\S+), (\S+)'
+        line_top = re.search(reg_ex, text_out[0])
+
+        reg_ex = r'^Tasks: +(\d+) total, +(\d+) running, +(\d+) sleeping, +(\d+) stopped, +(\d+) zombie'
+        line_tasks = re.search(reg_ex, text_out[1])
+
+        reg_ex = r'^%Cpu\(s\): +(\S+) us, +(\S+) sy, +(\S+) ni, +(\S+) id, +(\S+) wa, +(\S+) hi, +(\S+) si, +(\S+) st'
+        line_cpu = re.search(reg_ex, text_out[2])
+
+        reg_ex = r'^MiB Mem : +(\S+) total, +(\S+) free, +(\S+) used, +(\S+) buff/cache'
+        line_mem = re.search(reg_ex, text_out[3])
+
+        reg_ex = r'^MiB Swap: +(\S+) total, +(\S+) free, +(\S+) used. +(\S+) avail Mem'
+        line_swap = re.search(reg_ex, text_out[4])
+
+        # After parse and capturing all top info, reformat and give a summary.
+        # Capture bonus CPU info for displaying.
+        self.run_cpu(round(float(line_top.group(3)) * 100, 1))
+        
+        memory_used  = round((float(line_mem.group(3)) / float(line_mem.group(1)) \
+                             * 100), 1)
+        memory_free  = round((float(line_mem.group(2)) / 1024), 1)
+        memory_total = round((float(line_mem.group(1)) / 1024), 1)
+
+        display_memory = f'{memory_used}% Memory usage, ' \
+            f'({memory_free} GiB free, {memory_total} GiB total)'
+        print(display_memory)
+
+        self.run_disk()
 
     def run_disk(self):
         # TODO: See 'man ps' and look up what all should be collected on current
         # processes.
-        proc = subprocess.Popen(["ps", '-A'], 
+        proc = subprocess.Popen(["df", '-hl', '--total'], 
                                 stdout=subprocess.PIPE, 
                                 stderr=subprocess.STDOUT)
         # Check if the command failed to run correctly.
         if proc.returncode != 0:
             Exception(f"Error running 'ps -A': {proc.returncode}")
 
+        output = proc.communicate()
+
+        # TODO: Error handling (unexpected re failures).
+        text_out = re.split(r'\n', output[0].decode())
+
+        reg_ex = r'total\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)'
+        line = re.search(reg_ex, text_out[-2])
+
+        display_line = f'{line.group(4)} Storage usage, ' \
+            f'({line.group(3)} free, {line.group(1)} total)'
+        print(display_line)
         # TODO: Log procs/process command output.
-        # output = proc.communicate()
-        # print(output[0].decode())
+
--- projects/p0/options.py
+++ projects/p0/options.py
@@ -2,8 +2,22 @@ import re
 
 # Tool options, which will modify the behavior of the program.
 class Tools_Options():
+
+    # Special characters for formating printed strings.
+    f_error = '\033[91m'
+    f_warn  = '\033[93m'
+    f_bold  = '\033[1m'
+    f_uline = '\033[4m'
+    f_end   = '\033[0m'
+
     def __init__(self):
         # Script/Tool options:
+        
+        # TODO: Consider creating an options tuple for each option, that
+        # would combine the short/long commands needed to use them, and thier
+        # help info. Then use a set to store selected options for more 
+        # maintainable and readable code.
+
         # '--help' / '-h' The script will print to console how to use tools.py,
         # and explain all launch options.
         self.opt_help = False
@@ -19,6 +33,7 @@ class Tools_Options():
         self.opt_fast = False
         # '--verbose' / '-v' Prints and logs additional infomation.
         self.opt_verbose = False
+        # TODO: Commands to view, filter, and operate on the logs.
         # TODO: More as needed.
 
     # Returns a nice string for functions like print.
@@ -38,8 +53,62 @@ class Tools_Options():
             active.append("--verbose")
         return "Active options: " + str(active)
     
+    def help(self, cmd):
+        print("Runs SRE diagnostics and can deploy VMs to azure.")
+        print("")
+        print(self.f_bold + self.f_uline + "Usage:" + self.f_end + \
+              f" {cmd} + [OPTION]...")
+        print("")
+        # print(self.f_bold + self.f_uline + "Arguments:" + self.f_end + \
+        #       f" {cmd} + [TODO]...")
+        # print("")
+
+        def help_format_option(short, long, info):
+            c0_width = 4
+            c1_width = 42
+            # Format the short option in column 0.
+            c0 = ' ' * c0_width
+            if short:
+                c0 = c0[:c0_width - 2] + \
+                    f"-{self.f_bold}{short[0]}{self.f_end}"
+            # Choose correct separation.
+            if short and long:
+                c0 += ", "
+            else:
+                c0 += "  "
+            # Format long option in column 1.
+            c1 = ' ' * c1_width
+            if long:
+                c1 = f"--{self.f_bold}{long}{self.f_end}" + c1[len(long) + 2:]
+            print(c0 + c1 + info)
+        
+        print(self.f_bold + self.f_uline + "Options:" + self.f_end)
+        help_format_option('n', 'nolog', 
+                           'Skips writing infomation to the persistant DB.')
+        help_format_option('d', 'diagnostics', 
+                           'Only runs diagnostics, without creating any Azure resources.')
+        help_format_option('t', 'test', 
+                           'Creates a test VM that will run a HTTP server and validates functionality.')
+        help_format_option('f', 'fast', 
+                           'Autofills all user input, allow for easy testing.')
+        help_format_option('v', 'verbose', 
+                           'Prints and logs additional infomation.')
+        exit(0)
+
+    def invalid_arg(self, cmd, arg):
+        print(self.f_error + "error" + self.f_end + \
+              ": unexpected argument '" + \
+              self.f_warn + arg + self.f_end + \
+              "' found")
+        print("")
+        print(f"Usage: {cmd} [OPTION]...")
+        print("")
+        print("For more information, try '--help'.")
+        # Exit with command line/syntax error.
+        exit(2)
+    
     # Reads a list of arguments, and activates all the options found.
-    def update_from_args(self, args):
+    def update_from_args(self, cmd, args):
         for arg in args:
             # Using regular expressions, pull out any options.
             matches = re.search(r'^--(\w+)', arg)
@@ -47,9 +116,7 @@ class Tools_Options():
                 # Check what argument was provided.
                 arg_given = matches.group(1)
                 if re.search(r'^help$', arg_given, flags=re.IGNORECASE):
-                    # TODO: Print info.
-                    print("TODO: HELP INFO")
-                    exit()
+                    self.help(cmd)
 
                 elif re.search(r'^nolog$', arg_given, flags=re.IGNORECASE):
                     self.opt_nolog = True
@@ -67,17 +134,14 @@ class Tools_Options():
                     self.opt_verbose = True
 
                 else:
-                    # TODO: Invalid arg info.
-                    print(f"UNKNOWN ARG: --{arg_given}")
+                    self.invalid_arg(cmd, f"--{arg_given}")
             else:
                 matches = re.search(r'^-(\w+)', arg, flags=re.IGNORECASE)
                 if matches:
                     # Check what arguments were provided.
                     arg_given = matches.group(1)
                     if re.search(r'h', arg_given, flags=re.IGNORECASE):
-                        # TODO: Print info.
-                        print("TODO: HELP INFO")
-                        exit()
+                        self.help(cmd)
 
                     if re.search(r'n', arg_given, flags=re.IGNORECASE):
                         self.opt_nolog = True
@@ -100,8 +164,7 @@ class Tools_Options():
                         arg_given = re.sub(r'v', '', arg_given, flags=re.IGNORECASE)
 
                     if not re.search(r'^$', arg_given, flags=re.IGNORECASE):
-                        # TODO: Invalid args info.
-                        print(f"UNKNOWN ARGS: -{arg_given}")
+                        self.invalid_arg(cmd, f"-{arg_given[0]}")
 
 # Initalize the tool-options, which will be modified by the main function logic.
 active = Tools_Options()

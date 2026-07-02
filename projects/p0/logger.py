3687a0a285d8300d03834550813c5ae2b0279a80
Anonemous2 <AndrewFBates@icloud.com>
2026-07-02 13:47:34 -0400

    Tested DB

--- projects/p0/logger.py
+++ projects/p0/logger.py
@@ -11,11 +11,19 @@ import options
 # later easier.
 connect = sqlite3.connect('sre.db')
 cursor = connect.cursor()
+session_id = -1
+
+# Session log, so we can see which actions were taken during a particular
+# session.
+cursor.execute("""CREATE TABLE IF NOT EXISTS session_logs (
+               id INTEGER PRIMARY KEY,
+               time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+               );""")
 
 # Command log, so we can see which azure/bash commands where run, and
 # during which run of the program!
 cursor.execute("""CREATE TABLE IF NOT EXISTS command_logs (
-               id SERIAL PRIMARY KEY,
+               id INTEGER PRIMARY KEY,
                run_id,
                cmd VARCHAR(400),
                time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
@@ -23,7 +31,7 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS command_logs (
 
 # Records system performance accross runs.
 cursor.execute("""CREATE TABLE IF NOT EXISTS performance_logs (
-               id SERIAL PRIMARY KEY,
+               id INTEGER PRIMARY KEY,
                run_id,
                cmd VARCHAR(400),
                time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
@@ -32,14 +40,29 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS performance_logs (
 # Records the VMs and Azure resources created during runs, and saves the
 # commands needed to delete/cleanup resources later.
 cursor.execute("""CREATE TABLE IF NOT EXISTS vm_resources (
-               id SERIAL PRIMARY KEY,
+               id INTEGER PRIMARY KEY,
                run_id,
                cmd VARCHAR(400),
                time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
                );""")
 
+def start_session():
+    cursor.execute(f"INSERT INTO session_logs " \
+                   f"(id) VALUES" \
+                   f"(NULL)")
+    session_id = cursor.lastrowid
+    print(f"Session ID: {session_id}")
+
+    
+    cursor.execute(f"SELECT * FROM session_logs")
+    print(f"Sessions: {cursor.fetchall()}")
+
+    connect.commit()
+
 def insert_performance():
-    pass
+    cursor.execute(f"INSERT INTO performance_logs " \
+                   f"(TODO, TODO, TODO) VALUES" \
+                   f"({0}, {0}, {0})")
 
 def insert_command():
     pass
--- projects/p0/tools.py
+++ projects/p0/tools.py
@@ -4,6 +4,7 @@ import sys
 import options
 import diagnostics
 import deploy
+import logger
 
 # Checks if we're running this file as the main script, and if so, check CLI
 # arguments and execute the appropriate functions.
@@ -11,23 +12,33 @@ if __name__ == "__main__":
 
     # Update options from the passed CLI args.
     # Skip the first arg, as it's just the file name of this script.
-    options.active.update_from_args(sys.argv[1:])
+    options.active.update_from_args(sys.argv[0], sys.argv[1:])
 
     print("Running SRE diagnostics and deployment tools.")
     print(options.active)
-    print("---------------------------------------------")
 
     # Ok, run diagnostics on this machine.
     # TODO:
+    print(f"{options.Tools_Options.f_bold}{options.Tools_Options.f_uline}" \
+          f"System Statistics                            " \
+          f"{options.Tools_Options.f_end}")
     diagnostics = diagnostics.Diagnostics()
 
     # Exit program if -d flag.
     if options.active.opt_diagnostics:
+        print(f"{options.Tools_Options.f_bold}{options.Tools_Options.f_uline}" \
+              f"                                             " \
+              f"{options.Tools_Options.f_end}")
         print("Diagnostics complete, exiting program.")
-        exit()
+        exit(0)
 
     # Try to deploy.
     # TODO:
 
     # Successful Exit.
+    print(f"{options.Tools_Options.f_bold}{options.Tools_Options.f_uline}" \
+          f"                                             " \
+          f"{options.Tools_Options.f_end}")
     print("Deployment complete, exiting program.")
+
+    logger.start_session()
